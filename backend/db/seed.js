1009d86a8f913c315a7529923fbdc351c5c73e48
Sampreet <sam.learnnew@gmail.com>
2020-10-17 14:25:22 -0700

    Added code to upload seed data with db:seed node script

--- backend/db/seed.js
+++ backend/db/seed.js
@@ -1,3 +1,14 @@
-const mongoose = require('./connection')
-const Gifs = require("../models/gifs")
+const mongoose = require('./connection');
+const Gifs = require('../models/gifs');
+const seedData = require('./seed.json');
 
+const db = mongoose.connection;
+
+Gifs.deleteMany({})
+	.then(() => {
+		Gifs.insertMany(seedData)
+			.then((data) => console.log('Inserted Gifs: ', data))
+			.catch((err) => console.log('Error Inserting data: ', err))
+			.finally(() => db.close());
+	})
+	.catch((err) => console.log('Error deleting previous gifs'));
